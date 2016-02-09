07c52a88451f61c947297fcf1527edc93a4b5218
lccezinha <lccezinha@gmail.com>
2016-02-08 22:08:40 -0200

    Ajustes e finalização das ações de encurtar e redirecionar Url

--- server.go
+++ server.go
@@ -11,46 +11,73 @@ import (
 
 type Headers map[string]string
 
-/*
-Ainda é necessário implementar os exemplos do começo do capítulo (métodos do pacote URL)
-*/
-
 var (
   port int
   urlBase string
 )
 
 func init() {
-  port = "8080"
-  urlBase = fmt.Printf("http://locahost:%d", urlBase)
+  port = 8080
+  urlBase = fmt.Sprintf("http://locahost:%d", port)
 }
 
 func respondWith(w http.ResponseWriter, status int, headers Headers) {
   for key, value := range headers {
-    w.Header.Set(key, value)
+    w.Header().Set(key, value)
   }
 
   w.WriteHeader(status)
 }
 
-func extracUrl(r *http.Request) string {
+func extractUrl(r *http.Request) string {
   url := make([]byte, r.ContentLength, r.ContentLength)
   r.Body.Read(url)
   return string(url)
 }
 
-func Shorter(w http.ResponseWriter, r *http.Request) {
-  if r.Method != "POST" {
-    respondWith(w, http.StatusMethodNotAllowed, Headers{ "Allow":"POST", })
-    return
+func Redirecter(w http.ResponseWriter, r *http.Request) {
+  path := strings.Split(r.URL.Path, "/")
+  id := path[len(path) - 1]
+
+  if url := url.Find(id); url != nil {
+    http.Redirect(w, r, url.UrlOriginal, http.StatusMovedPermanently)
+  } else {
+    http.NotFound(w, r)
   }
 }
 
-func main() {
-  http.HandleFunc("/api/short", Shorter)
-  http.HandleFunc("/r/", Redirecter)
+func Shorter(w http.ResponseWriter, r *http.Request) {
+  if r.Method != "POST" {
+    respondWith(w, http.StatusMethodNotAllowed, Headers{"Allow":"POST"})
+    return
+  }
 
-  log.Fatal(
-    http.ListenAndServe(fmt.Printf(":%d", port), nil)
-  )
+  url, isNew, err := url.FetchUrl(extractUrl(r))
+
+  if err != nil {
+    respondWith(w, http.StatusBadRequest, nil)
+    return
+  }
+
+  var status int
+  if isNew {
+    status = http.StatusCreated
+  } else {
+    status = http.StatusOK
+  }
+
+  urlShort := fmt.Sprintf("%s/r/%s", urlBase, url.Id)
+  respondWith(w, status, Headers{"Location": urlShort})
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello")
+}
+
+func main() {
+  http.HandleFunc("/api/shorter", Shorter)
+  http.HandleFunc("/r/", Redirecter)
+  http.HandleFunc("/home", Home)
+
+  log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }

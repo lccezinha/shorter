4675f9d36e725026cc6ee01410357007c965eab3
lccezinha <lccezinha@gmail.com>
2016-02-16 21:21:01 -0200

    Impl. básica para repositório com redis

--- url/redis_repo.go
+++ url/redis_repo.go
@@ -1,12 +1,33 @@
 package url
 
-type redisRepository struct{}
+import (
+  "gopkg.in/redis.v3"
+  "strconv"
+  "strings"
+)
+
+type redisRepository struct {
+  client *redis.Client
+  urls map[string]*Url
+}
 
 func InitializeRedisRepository() *redisRepository {
-  return &redisRepository{}
+  client := redis.NewClient(&redis.Options{
+    Addr: ":6379",
+    Password: "",
+    DB: 0,
+  })
+
+  return &redisRepository{
+    client,
+    make(map[string]*Url),
+  }
 }
 
 func (r *redisRepository) Persisted(id string) bool {
+  // key := strings.Join([]string{"urls", url.Id}, ":")
+
+  // r.client.HExists(key)
 }
 
 func (r *redisRepository) FindById(id string) *Url {
@@ -16,10 +37,23 @@ func (r *redisRepository) FindByUrl(url string) *Url {
 }
 
 func (r *redisRepository) Save(url Url) error {
+  // key := strings.Join([]string{"urls", url.Id}, ":")
+  // r.client.HSet(key, url.UrlOriginal)
+
+  // return nil
 }
 
 func (r *redisRepository) RegisterClick(id string) {
+  r.client.HSet("clicks", id, "1")
 }
 
 func (r *redisRepository) ShowClicks(id string) int {
+  clicks := r.client.HGet("clicks", id).Val()
+  i, err := strconv.Atoi(clicks)
+
+  if err != nil {
+    return i
+  }
+
+  return 0
 }

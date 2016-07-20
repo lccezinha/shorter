803478a63e628f78dc88fab02b77a87f9a419e3c
Luiz Cezer <lccezinha@gmail.com>
2016-07-19 21:08:56 -0300

    Salvando dados no Mongo

--- url/mongo_repo.go
+++ url/mongo_repo.go
@@ -1,18 +1,37 @@
 package url
 
 import (
+	"fmt"
 	"gopkg.in/mgo.v2"
+	"time"
 )
 
+var urlConnection = "localhost:27017"
+
+const database = "shorter"
+const collection = "urls"
+
 type mongoRepository struct {
+	session *mgo.Session
 }
 
 func InitializeMongoRepository() *mongoRepository {
-	return mongoRepository
+	session, err := mgo.Dial(urlConnection)
+
+	if err != nil {
+		panic("Database connection Error!")
+	}
+
+	session.SetMode(mgo.Monotonic, true)
+
+	return &mongoRepository{session}
 }
 
 func (mr *mongoRepository) Persisted(id string) bool {
-	return nil
+	return false
+	// var u Url
+
+	// mr.connection.DB(database).C(collection)
 }
 
 func (mr *mongoRepository) FindById(id string) *Url {
@@ -24,11 +43,18 @@ func (mr *mongoRepository) FindByUrl(url string) *Url {
 }
 
 func (mr *mongoRepository) Save(url Url) error {
+	u := &Url{id: url.Id, created_at: time.Now(), url_original: url.UrlOriginal}
+
+	err := mr.session.DB(database).C(collection).Insert(u)
+
+	if err != nil {
+		panic(err)
+	}
+
 	return nil
 }
 
 func (mr *mongoRepository) RegisterClick(id string) {
-	return nil
 }
 
 func (mr *mongoRepository) ShowClicks(id string) int {

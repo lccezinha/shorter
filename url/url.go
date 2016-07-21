e98e2163a344cc8bdce042830a933794f4119b26
Luiz Cezer <lccezinha@gmail.com>
2016-07-20 21:26:54 -0300

    Adição de bson package e ajustes no URL struct

--- url/url.go
+++ url/url.go
@@ -1,89 +1,91 @@
 package url
 
 import (
-  "math/rand"
-  "net/url"
-  "time"
+	"labix.org/v2/mgo/bson"
+	"math/rand"
+	"net/url"
+	"time"
 )
 
 const (
-  size = 5
-  symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-+"
+	size    = 5
+	symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-+"
 )
 
 var repository Repository
 
 type Repository interface {
-  Persisted(id string) bool
-  FindById(id string) *Url
-  FindByUrl(url string) *Url
-  Save(url Url) error
-  RegisterClick(id string)
-  ShowClicks(id string) int
+	Persisted(id string) bool
+	FindById(id string) *Url
+	FindByUrl(url string) *Url
+	Save(url Url) error
+	RegisterClick(id string)
+	ShowClicks(id string) int
 }
 
 type Url struct {
-  Id          string    `json:"id"`
-  CreatedAt   time.Time `json:"created_at"`
-  UrlOriginal string    `json:"url_original"`
+	Mid         bson.ObjectId `bson:"_id,omitempty"`
+	Id          string        `bson:"id,omitempty" json:"id"`
+	CreatedAt   time.Time     `bson:"created_at" json:"created_at"`
+	UrlOriginal string        `bson:"url_original" json:"url_original"`
 }
 
 type Stats struct {
-  Url   *Url `json:"url"`
-  Clicks int `json:"clicks"`
+	Url    *Url `json:"url"`
+	Clicks int  `json:"clicks"`
 }
 
 func ConfigRepository(r Repository) {
-  repository = r
+	repository = r
 }
 
 func init() {
-  rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 }
 
 func generateId() string {
-  newId := func() string {
-    id := make([]byte, size, size)
+	newId := func() string {
+		id := make([]byte, size, size)
 
-    for i := range id {
-      id[i] = symbols[rand.Intn(len(symbols))]
-    }
+		for i := range id {
+			id[i] = symbols[rand.Intn(len(symbols))]
+		}
 
-    return string(id)
-  }
+		return string(id)
+	}
 
-  for {
-    if id := newId(); !repository.Persisted(id) {
-      return id
-    }
-  }
+	for {
+		if id := newId(); !repository.Persisted(id) {
+			return id
+		}
+	}
 }
 
 func Find(id string) *Url {
-  return repository.FindById(id)
+	return repository.FindById(id)
 }
 
 func RegisterClick(id string) {
-  repository.RegisterClick(id)
+	repository.RegisterClick(id)
 }
 
 func FetchUrl(urlOriginal string) (u *Url, isNew bool, err error) {
-  if u = repository.FindByUrl(urlOriginal); u != nil {
-    return u, false, nil
-  }
+	if u = repository.FindByUrl(urlOriginal); u != nil {
+		return u, false, nil
+	}
 
-  if _, err = url.ParseRequestURI(urlOriginal); err != nil {
-    return nil, false, err
-  }
+	if _, err = url.ParseRequestURI(urlOriginal); err != nil {
+		return nil, false, err
+	}
 
-  url := Url{generateId(), time.Now(), urlOriginal}
-  repository.Save(url)
+	url := Url{Id: generateId(), CreatedAt: time.Now(), UrlOriginal: urlOriginal}
+	repository.Save(url)
 
-  return &url, true, nil
+	return &url, true, nil
 }
 
 func (u *Url) ShowStats() *Stats {
-  clicks := repository.ShowClicks(u.Id)
+	clicks := repository.ShowClicks(u.Id)
 
-  return &Stats{u, clicks}
+	return &Stats{u, clicks}
 }
